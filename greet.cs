670976854e9d69853080cee020fb53d116ebc1cb
Tuan Anh <tuananhbgbg@gmail.com>
2020-11-23 14:22:17 +0100

    a2

--- greet.cs
+++ greet.cs
@@ -1,3 +1,6 @@
-System.Console.WriteLine("Anh");
-var parameter = 0;
-System.Console.WriteLine(parameter);
+public static void Greet(int parameter)
+{
+    System.Console.WriteLine("Anh");
+    var parameter = 0;
+    System.Console.WriteLine(parameter);
+}

2b0f0f80ae8e344a7f0b13abd1d13fc7cda948e8
Marketa Hajickova <marketahajickova@gmail.com>
2020-11-23 14:50:31 +0100

    b6

--- greet.cs
+++ greet.cs
@@ -6,7 +6,7 @@ public class Greet
     {
         System.Console.WriteLine("Anh");
         var parameter = 0;
-        System.Console.WriteLine(parameter);
+        System.Console.WriteLine("Greetings for the "+parameter);
         
     }
     public static void Count(){

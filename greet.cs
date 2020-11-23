32d6948e718541d523474825cc96224c7438a57e
Tuan Anh <tuananhbgbg@gmail.com>
2020-11-23 14:37:53 +0100

    a5

--- greet.cs
+++ greet.cs
@@ -2,14 +2,11 @@ namespace Greet
 {
 public class Greet
 {
-    public static void Greet(int parameter)
+    public static void GreetCount(int parameter,int birthdate)
     {
         System.Console.WriteLine("Anh");
         var parameter = 0;
         System.Console.WriteLine(parameter);
-        
-    }
-    public static void Count(){
-        System.Console.WriteLine(7);
+        System.Console.WriteLine(birthdate);
     }
 }}

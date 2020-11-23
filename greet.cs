f00a0f04668de8f88032c2b74781f87e57683fdf
Tuan Anh <tuananhbgbg@gmail.com>
2020-11-23 14:24:43 +0100

    a3

--- greet.cs
+++ greet.cs
@@ -1,6 +1,15 @@
-public static void Greet(int parameter)
+namespace Greet
 {
-    System.Console.WriteLine("Anh");
-    var parameter = 0;
-    System.Console.WriteLine(parameter);
-}
+public class Greet
+{
+    public static void Greet(int parameter)
+    {
+        System.Console.WriteLine("Anh");
+        var parameter = 0;
+        System.Console.WriteLine(parameter);
+        
+    }
+    public static void Count(){
+        System.Console.WriteLine(7);
+    }
+}}
